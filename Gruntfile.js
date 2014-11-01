ecd077845c242e3d35ca106a3ef65c97a3098f9b
Tom Cowling <tom.cowling@gmail.com>
2014-11-01 12:09:59 +0000

    Rename src to assets (its the convention it seems)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,42 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
+  require('load-grunt-tasks')(grunt);
+
+  grunt.config.init({
     pkg: grunt.file.readJSON('package.json'),
+    concat: { 
+      generated: { 
+        files: [ 
+          { 
+             dest: '.tmp/concat/assets/css/style.min.css', 
+             src: [ 'assets/css/style.css' ] 
+          },
+          { 
+             dest: '.tmp/concat/assets/js/optimized.js',
+             src: [ 'assets/js/foo.js', 'assets/js/bar.js' ] 
+          } 
+        ] 
+      } 
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+    usemin: {
+      html: [ 'dist/index.html' ]
+    },
+    copy: {
+      html: {
+        src: './index.html', dest: 'dist/index.html'
+      }
+    },
     jshint: {
       files: [ 'Gruntfile.js', 'src/**/*.js', 'test/**/*.js' ]
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('build', ['copy:html','useminPrepare','concat','uglify','cssmin','usemin']);
 
 };
