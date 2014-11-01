5db74b40796b43a2822cac204852a4dcbe6736ad
Tom Cowling <tom.cowling@gmail.com>
2014-11-01 13:33:29 +0000

    Remove usemin:prepare task, I want to specify the structure myself

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
           expand: true,
           cwd   : 'assets/images',
           src   : ['**/*.{png,jpg,gif}'],
-          dest: 'dist/assets/images'
+          dest: 'dist/images'
         }]
       }
     },
@@ -17,11 +17,11 @@ module.exports = function(grunt) {
       generated: { 
         files: [ 
           { 
-             dest: '.tmp/concat/assets/css/style.min.css', 
+             dest: '.tmp/concat/css/style.min.css', 
              src: [ 'assets/css/style.css', 'assets/css/print.css' ] 
           },
           { 
-             dest: '.tmp/concat/assets/js/optimized.js',
+             dest: '.tmp/concat/js/optimized.js',
              src: [ 'assets/js/general.js', 'assets/js/jquery.min.js' ] 
           } 
         ] 
@@ -31,8 +31,8 @@ module.exports = function(grunt) {
       generated: { 
         files: [ 
           { 
-            dest: 'dist/assets/js/optimized.js',
-            src: [ '.tmp/concat/assets/js/optimized.js' ] 
+            dest: 'dist/js/optimized.js',
+            src: [ '.tmp/concat/js/optimized.js' ] 
           } 
         ] 
       } 
@@ -41,18 +41,18 @@ module.exports = function(grunt) {
       generated: {
         files: [
           { 
-            dest: 'dist/assets/css/style.min.css',
-            src : [ '.tmp/concat/assets/css/style.min.css' ] 
+            dest: 'dist/css/style.min.css',
+            src : [ '.tmp/concat/css/style.min.css' ] 
           }
         ]
       }
     },
-    useminPrepare: {
-      html: 'assets/index.html',
-      options: {
-        dest: 'dist'
-      }
-    },
+    
+    
+    
+    
+    
+    
     usemin: {
       html: [ 'dist/index.html' ]
     },
@@ -80,9 +80,7 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default', ['build']);
-  grunt.registerTask('build', ['jshint','copy:html','useminPrepare','concat','uglify','cssmin','usemin']);
-
-  grunt.registerTask('compressimages', ['imagemin']);
-  grunt.registerTask('deploy', ['build','rsync:prod']);
+  grunt.registerTask('build', ['jshint','copy:html','concat','uglify','cssmin','usemin']);
+  grunt.registerTask('deploy', ['build','imagemin','rsync:prod']);
 
 };
