263231cd756cc24d3ed88c3faf20185cf4edf878
Tom Cowling <tom.cowling@gmail.com>
2015-05-19 10:03:53 -0700

    Replace Grunt with gulp, make changes to the script for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,58 @@ var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
+var rev = require('gulp-rev');
+var minifyCss = require('gulp-minify-css');
+var minifyHtml = require('gulp-minify-html');
+var usemin = require('gulp-usemin');
+var rsync = require('gulp-rsync');
 
 var paths = {
+  build: 'build/**',
+  scripts: 'site/js/**/*.js',
   images: 'site/img/**/*'
 };
 
-// Not all tasks need to use streams
-// A gulpfile is just another node program and you can use all packages available on npm
+gulp.task('usemin', function() {
+  return gulp.src('./site/*html')
+     .pipe(usemin({
+       css: [minifyCss(), 'concat'],
+       html: [minifyHtml({empty:true})],
+       js: [uglify(), rev()],
+       inlinejs: [uglify()],
+       inlinecss: [minifyCss(), 'concat']
+     }))
+     .pipe(gulp.dest('build/'));
+});
+
 gulp.task('clean', function(cb) {
   // You can use multiple globbing patterns as you would with `gulp.src`
   del(['build'], cb);
 });
 
-
-gulp.task('default', function() {
-
+gulp.task('scripts', ['clean'], function() {
+  return gulp.src(paths.scripts)
+             .pipe(sourcemaps.init())
+             .pipe(uglify())
+             .pipe(concat('all.min.js'))
+             .pipe(sourcemaps.write())
+             .pipe(gulp.dest('build/js'));
 });
+
+gulp.task('images', ['clean'], function() {
+  return gulp.src(paths.images)
+             .pipe(imagemin({optimizationLevel: 5}))
+             .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('deploy', ['usemin'], function() {
+  return gulp.src(paths.build)
+             .pipe(rsync({
+                   username: 'deployer',
+                   root: 'build',
+                   hostname: 'tlcowling.com',
+                   destination: '/var/www/tlcowling.com/profile'
+                 }));     
+});
+
+gulp.task('default', ['scripts', 'images'], function() { });
